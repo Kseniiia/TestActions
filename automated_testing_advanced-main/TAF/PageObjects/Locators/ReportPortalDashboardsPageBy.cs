5b16fef8f388b7cbde906bd397e8369de51606d8
Kseniya Malchykava <45138699+Kseniiia@users.noreply.github.com>
2025-07-25 10:35:13 +0200

    Delete automated_testing_advanced-main/TAF directory

--- automated_testing_advanced-main/TAF/PageObjects/Locators/ReportPortalDashboardsPageBy.cs
+++
@@ -1,9 +0,0 @@
-using OpenQA.Selenium;
-
-namespace PageObjects.Locators
-{
-    public class ReportPortalDashboardsPageBy
-    {
-        public static By LoginPopup = By.XPath("//div[@class='notification-transition-enter-done']");
-    }
-}
